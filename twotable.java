b536dd73455fad06a6a07e36d16c6ca88af8c0a6
jvraviteja18 <jvraviteja18@gmail.com>
2021-02-15 15:34:23 +0530

    commit 5

---
+++ twotable.java
@@ -0,0 +1,12 @@
+import java.io.*;
+class helloworld
+{
+	public static void main(String[]args)
+	{
+		System.out.println("Hello world!");
+		for(int i=0;i<10;i++)
+		{
+			System.out.println(2*i+"\n");
+		}
+	}
+}
